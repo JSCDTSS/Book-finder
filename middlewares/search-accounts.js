2638f9ab3ffb2b1e9ec09711bfda625a11a319ec
SamMatthews96 <sam-matthews@hotmail.co.uk>
2022-10-26 09:50:35 +0100

    fixed bug with getting accounts

--- middlewares/search-accounts.js
+++ middlewares/search-accounts.js
@@ -1,7 +1,8 @@
 
 
 module.exports = async function (req, res) {
+  const { database } = req.app.locals.settings
   const accounts = await database.getAccounts(req.query)
   console.log(accounts)
-  res({ok:true})
+  res.json({ok:true})
 }
