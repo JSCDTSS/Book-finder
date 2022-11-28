db4847c2ba5170a9560bd1b200773e5791a042fd
JACK GREENWOOD <b8020804@hallam.shu.ac.uk>
2022-11-28 17:51:08 +0000

    Revert "Attempting to fix merge"
    This reverts commit c9500b8e34e70ed6a0a5eaac86d858174a515d04.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import {useLocation} from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import '../Master.css';
 import Bell from '../icons/bell.svg';
