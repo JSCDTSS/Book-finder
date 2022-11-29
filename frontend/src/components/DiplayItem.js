345e73f8031b1214beeef53805b6e0de311a59a2
JACK GREENWOOD <b8020804@hallam.shu.ac.uk>
2022-11-29 14:15:38 +0000

    Improved modal boxes again.

--- frontend/src/components/DiplayItem.js
+++ frontend/src/components/DiplayItem.js
@@ -30,15 +30,15 @@ export default function DisplayItem({ item }) {
     <img src={item?.imageLinks?.smallThumbnail} alt={item.alt} onClick={handleOpen} />
 
     <Modal
-      open={open}
-      onClose={handleClose}
-    >
-      <Box sx={style}>
-        <h2>{item.title}</h2>
-        <p>{item.description}</p>
-        <button onClick={handleClose}>Close</button>
-        <img src={item?.imageLinks?.thumbnail} alt={item.alt} />
-      </Box>
-    </Modal>
+        open={open}
+        onClose={handleClose}
+      >
+        <Box className='ModalBox'>
+          <button className='ModalClose' onClick={handleClose}>Close</button>
+          <h4>{item.title}</h4>
+          <p>{item.description}</p>
+        </Box>
+      </Modal>
   </div>
 }
+
